6430b4a5aba904b2dfdde79660a7aa4eb658c361
WinyRangel <99991728+WinyRangel@users.noreply.github.com>
2023-03-26 22:22:17 -0600

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI ='mongodb://localhost/contenido_f';
-
-mongoose.connect(URI)
-.then(db => console.log('DB is connected'))
-.catch(err => console.console.error(err));
-module.exports = mongoose;
